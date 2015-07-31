be730b951e209ac110f59a75ebeaa184e7312b52
Masatoshi Kanamaru <a1520mk@aiit.ac.jp>
2015-07-31 12:53:03 +0000

    add test

--- test/a1520mk_exercise4_test.rb
+++ test/a1520mk_exercise4_test.rb
@@ -5,7 +5,13 @@ class A1520mkExercise4Test < Minitest::Test
     refute_nil ::A1520mkExercise4::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_getAge
+    refute_equal 52, A1520mkExercise4::A1520mkExercise4.getAge
+    assert_equal 53, A1520mkExercise4::A1520mkExercise4.getAge
+    refute_equal 54, A1520mkExercise4::A1520mkExercise4.getAge
+  end
+
+  def test_getBMI
+    assert_equal 12.5, A1520mkExercise4::A1520mkExercise4.getBMI(50,200)
   end
 end
